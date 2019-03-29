9d21264ad6ef9f9a99292233c82bd42d571b64b1
Pankaj Sharma <pankaj.sharma@nearbuy.com>
2019-03-29 12:16:59 +0530

    Initial Commit

---
+++ PSQRcodeGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PSQRcodeGenerator
+//
+//  Created by Pankaj Sharma on 29/03/19.
+//  Copyright © 2019 idevios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PSQRcodeGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PSQRcodeGenerator
+//
+//  Created by Pankaj Sharma on 29/03/19.
+//  Copyright © 2019 idevios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
